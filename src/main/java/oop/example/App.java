1c561ec1f9d7ef387e45cd06f27216956d015d19
kingraham228 <kate.ingraham@knights.ucf.edu>
2021-06-03 11:56:58 -0400

    Beginning setup.

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -5,6 +5,20 @@ package oop.example;
  *  Copyright 2021 Kate Ingraham
  */
 
+/**Example Output
+ Is the car silent when you turn the key? y
+ Are the battery terminals corroded? n
+ The battery cables may be damaged.
+ Replace cables and try again.
+
+ Constraint
+ Ask only questions that are relevant to the situation and to the previous answers. Don’t ask for all inputs at once.
+
+ Challenge
+ Investigate rules engines and inference engines.
+ These are powerful ways to solve complex problems that are based on rules and facts.
+ Identify a rules engine for your programming language and use it to solve this problem*/
+
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/oop/example/Car.java
@@ -0,0 +1,9 @@
+package oop.example;
+
+public class Car {
+   private String silent;
+   private String terminals;
+   private String sNoise;
+   private String crankNoStart;
+   private String fuelInjection;
+}
