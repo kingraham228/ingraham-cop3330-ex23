fb97165906ab37404df7916f556f2d8ac25ed961
kingraham228 <kate.ingraham@knights.ucf.edu>
2021-06-03 14:36:17 -0400

    Get answer function implement.

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -23,7 +23,8 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        Car myCar = new Car();
+        myCar.diagnose();
 
     }
 }
--- src/main/java/oop/example/Car.java
+++ src/main/java/oop/example/Car.java
@@ -1,9 +1,38 @@
 package oop.example;
 
+import java.util.Scanner;
+
 public class Car {
+   Scanner input = new Scanner(System.in);
+   private String question;
    private String silent;
    private String terminals;
    private String sNoise;
    private String crankNoStart;
    private String fuelInjection;
+
+   public String getYesNo(String q){
+      String answer;
+      int loop = 0;
+
+      do {
+         System.out.println(q);
+         answer = input.next();
+         if(answer.equalsIgnoreCase("Yes")||answer.equalsIgnoreCase("No")
+                 ||answer.equalsIgnoreCase("Y")||answer.equalsIgnoreCase("N")){
+            loop = 1;
+         }else {
+            System.out.println("Response not recognized. Please type \"yes\" or \"no\".");
+         }
+
+      } while (loop<1);
+
+    return answer;
+   }
+
+   public void diagnose(){
+      question = "Is the car silent when you turn the key?";
+      silent = getYesNo(question);
+      System.out.println(silent);
+   }
 }
